1e1b9ac94ca4d6c99bf93d2b1f73f47f65bdde75
swasty jay <amekpoagbedaniel@gmail.com>
2024-12-01 13:14:18 +0000

    starting the project

--- script.js
+++ script.js
@@ -65,12 +65,47 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+// let arr = [23, 45, 78];
+// console.log(arr[0]);
+// console.log(arr.at(1));
+
+//NEW ARRAY METHODS/////////
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// // for (const movement of movements) {
+// for (const [i, movement] of movements.entries()) {
+//   if (movement > 0) {
+//     console.log(`movemont ${i + 1}: you deposited ${movement}`);
+//     // console.log(` movement ${i + 1}: you deposited ${movement}`);
+//   } else {
+//     console.log(`movemont ${i + 1}: you withdraw ${Math.abs(movement)}`);
+//   }
+// }
+// console.log('_____FOREACH_____');
+// movements.forEach(function (movement, i, arr) {
+//   if (movement > 0) {
+//     console.log(`movemont ${i + 1}: you deposited ${movement}`);
+//     // console.log(` movement ${i + 1}: you deposited ${movement}`);
+//   } else {
+//     console.log(`movemont ${i + 1}: you withdraw ${Math.abs(movement)}`);
+//   }
+// });
+
+// ///FOREACH WITH MAPS AND SET////////////////////////
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+// currencies.forEach(function (value, key, map) {
+//   console.log(`${key}: ${value}`);
+// });
